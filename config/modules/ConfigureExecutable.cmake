19f0a8f6e302596169c95bdab15e8ddce0cc8f8e
Jason Monschke <jam4375@gmail.com>
2021-04-17 18:30:19 -0700

    Add 'configure_executable()' cmake function

---
+++ config/modules/ConfigureExecutable.cmake
@@ -0,0 +1,19 @@
+function(configure_executable)
+    set(oneValueArgs NAME SOURCE_DIR)
+    set(multiValueArgs SOURCE_FILES BUILD_LINK_LIBRARIES)
+    cmake_parse_arguments(EXE "${options}" "${oneValueArgs}" "${multiValueArgs}" ${ARGN})
+
+    set(ExecutableTargetName ${PROJECT_NAME}_${EXE_NAME})
+
+    message(
+        STATUS
+            "Configuring library ${EXE_NAME} with target \"${ExecutableTargetName}\" and alias \"${PROJECT_NAME}::${EXE_NAME}\""
+    )
+
+    add_executable(${ExecutableTargetName} ${EXE_SOURCE_FILES})
+    add_executable(${PROJECT_NAME}::${EXE_NAME} ALIAS ${ExecutableTargetName})
+
+    target_include_directories(${ExecutableTargetName} PRIVATE ${CMAKE_CURRENT_SOURCE_DIR}/${EXE_SOURCE_DIR})
+
+    target_link_libraries(${ExecutableTargetName} PRIVATE ${EXE_BUILD_LINK_LIBRARIES})
+endfunction()
--- config/modules/modules.cmake
+++ config/modules/modules.cmake
@@ -3,3 +3,4 @@ list(APPEND CMAKE_MODULE_PATH "${CMAKE_CURRENT_LIST_DIR}")
 
 # Add custom cmake modules:
 include(ConfigureLibrary)
+include(ConfigureExecutable)
