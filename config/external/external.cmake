67881f37dc9c47deb075cc8736f7c8ad93ca2b4c
Jason Monschke <jam4375@gmail.com>
2021-04-23 20:46:05 -0700

    CMake tweak

--- config/external/external.cmake
+++ config/external/external.cmake
@@ -10,4 +10,4 @@ include("${CMAKE_CURRENT_LIST_DIR}/cxxopts.cmake")
 # Restore clang-tidy configuration:
 set(CMAKE_CXX_CLANG_TIDY
     ${CMAKE_CXX_CLANG_TIDY_OLD}
-    CACHE BOOL "Re-enable clang-tidy for templo" FORCE)
+    CACHE BOOL "Re-enable clang-tidy for ${PROJECT_NAME}" FORCE)
